847bb9f32926d30b1f8988adf2934677f09d74b7
EpicKiwi <lelycandu76@gmail.com>
2015-04-25 18:57:15 +0200

    Animation lors de la connection

--- static/js/app.js
+++ static/js/app.js
@@ -7,17 +7,41 @@ app.factory("userFactory",function(){
 	}
 })
 
+app.factory("graphicalFactory",function(){
+	return {
+		backgroundBlur: false,
+	}
+});
+
 app.factory("socket",function(socketFactory){
 	return socketFactory();
 });
 
-app.controller("globalController",function($scope){
-	$scope.backgroundBlur = true;
+app.controller("globalController",function($scope,graphicalFactory){
+	$scope.backgroundBlur = function(){
+		return graphicalFactory.backgroundBlur;
+	}
 });
 
-app.controller("loginController",function($scope,userFactory,socket){
+app.controller("loginController",function($scope,userFactory,socket,graphicalFactory){
+
+	function boxOut(){
+		var container = jQuery('#login-form');
+		var title = container.find("h1");
+		var box = container.find(".logBox");
+
+		var timeline = new TimelineMax();
+		timeline.to(box,0.2,{opacity:0,scale:0.5,boxShadow:"0 0 0 black"});
+		timeline.to(title,0.5,{y: 100,scale:1.5},"-=0.1");
+		timeline.to(title,0.5,{opacity:0},"+=3");
+		timeline.to(container,0,{display:"none"});
+	};
+
+	graphicalFactory.backgroundBlur = true;
 	$scope.loading = false;
 	$scope.formError = false;
+	$scope.isLogged = false;
+
 	$scope.logIn = function(){
 		socket.emit("logIn",{username:$scope.username,password:$scope.password});
 		socket.once("logIn",function login(data){
@@ -31,6 +55,9 @@ app.controller("loginController",function($scope,userFactory,socket){
 			{
 				userFactory.status = "connected";
 				userFactory.username = data.username;
+				$scope.isLogged = true;
+				graphicalFactory.backgroundBlur = false;
+				boxOut();
 			}
 		});
 
