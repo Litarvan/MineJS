85f064fd55fde2f09bdffcbbefb9886f83561f6e
EpicKiwi <lelycandu76@gmail.com>
2015-04-25 12:40:58 +0200

    Premier serveur web

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use("/static",express.static("/static"));
+app.use("/partials",express.static("/core/partials"));
+
+app.get("/",function(request,response){
+	response.sendFile(__dirname+"/core/partials/index.html");
+});
+
+io.on("connection",function(socket){
+	console.log("Conexion socket");
+});
+
+http.listen(80);
