15c8ec2ecff8ae215fc6101714fb1477ad49dd91
EpicKiwi <lelycandu76@gmail.com>
2015-04-27 01:08:21 +0200

    Ajustements

--- apps/setup/static/global.js
+++ apps/setup/static/global.js
@@ -1,3 +1,11 @@
 app.controllerProvider.register("setupAppController",function($scope){
 	$scope.tab = 1;
+
+	$scope.nextStep = function(){
+		$scope.tab++;
+	};
+
+	$scope.previousStep = function(){
+		$scope.tab--;
+	};
 });
