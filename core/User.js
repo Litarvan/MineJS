ff94e1e9b200e7f18e130e4d255fbedf2ce85271
EpicKiwi <lelycandu76@gmail.com>
2015-04-26 22:57:34 +0200

    Resolution de bug utilisateur global

--- core/User.js
+++ core/User.js
@@ -2,88 +2,89 @@ var yaml = require("../node_modules/js-yaml");
 var fs = require("fs");
 var crypto = require('crypto');
 
-var user = {
-
-	trusted: false,
-	infos : {
-			username: null,
-			password: null,
-			},
-	socket: null,
-
-	setPassword: function(password)
-	{
-		this.infos.password = this.hashString(password);
-	},
-
-	hashString: function(string)
-	{
-		var shahash = crypto.createHash('sha1');
-		shahash.update(string);
-		return shahash.digest('hex');
-	},
-
-	save: function(){
-		var users = yaml.safeLoad(fs.readFileSync('./config/users.yml', 'utf8'));
-
-		if(typeof users != "array")
-		{
-			users = [];
-		}
-
-		for(var i = 0; i<users.length; i++)
-		{
-			if(users[i].username == this.infos.username)
-			{
-			  	users[i].password = this.infos.password;
-			  	break;
-			}
-		}
-
-		if(i == users.length)
-		{
-			users.push(this.infos);
-		}
-
-		fs.unlinkSync('./config/users.yml');
-		fs.writeFile('./config/users.yml', yaml.dump(users), function (err) {
-		  if (err) throw err;
-		});
-	},
-
-	check: function(){
-		if(typeof this.infos.username !== 'undefined' && typeof this.infos.password !== 'undefined')
-		{
-			try 
-			{
-			  var users = yaml.safeLoad(fs.readFileSync('./config/users.yml', 'utf8'));
-			  for(var i = 0; i<users.length; i++)
-			  {
-			  	if(users[i].username == this.infos.username && users[i].password == this.infos.password)
-			  	{
-			  		this.trusted = true;
-			  		this.infos.username = users[i].username;
-			  		this.infos.password = users[i].password;
-			  		return;
-			  	}
-			  }
-			  this.trusted = false;
-			} 
-			catch (e) 
-			{
-			  console.log(e);
-			  this.trusted = false;
-			}
-		}
-		else
-		{
-			this.trusted = false;
-		}
-	},
-
-}
-
 module.exports = function(socket){
+	var user = {
+
+		trusted: false,
+		infos : {
+				username: null,
+				password: null,
+				},
+		socket: null,
+		activeApp: null,
+
+		setPassword: function(password)
+		{
+			this.infos.password = this.hashString(password);
+		},
+
+		hashString: function(string)
+		{
+			var shahash = crypto.createHash('sha1');
+			shahash.update(string);
+			return shahash.digest('hex');
+		},
+
+		save: function(){
+			var users = yaml.safeLoad(fs.readFileSync('./config/users.yml', 'utf8'));
+
+			if(typeof users != "array")
+			{
+				users = [];
+			}
+
+			for(var i = 0; i<users.length; i++)
+			{
+				if(users[i].username == this.infos.username)
+				{
+				  	users[i].password = this.infos.password;
+				  	break;
+				}
+			}
+
+			if(i == users.length)
+			{
+				users.push(this.infos);
+			}
+
+			fs.unlinkSync('./config/users.yml');
+			fs.writeFile('./config/users.yml', yaml.dump(users), function (err) {
+			  if (err) throw err;
+			});
+		},
+
+		check: function(){
+			if(typeof this.infos.username !== 'undefined' && typeof this.infos.password !== 'undefined')
+			{
+				try 
+				{
+				  var users = yaml.safeLoad(fs.readFileSync('./config/users.yml', 'utf8'));
+				  for(var i = 0; i<users.length; i++)
+				  {
+				  	if(users[i].username == this.infos.username && users[i].password == this.infos.password)
+				  	{
+				  		this.trusted = true;
+				  		this.infos.username = users[i].username;
+				  		this.infos.password = users[i].password;
+				  		return;
+				  	}
+				  }
+				  this.trusted = false;
+				} 
+				catch (e) 
+				{
+				  console.log(e);
+				  this.trusted = false;
+				}
+			}
+			else
+			{
+				this.trusted = false;
+			}
+		},
+
+	}
+
 	if(typeof socket != 'undefined')
 	{
 		user.socket = socket;
